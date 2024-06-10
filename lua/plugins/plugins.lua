21717ab3890c854385d84ee36aa3fbc45d6cc326
goerll <estevaogoerlln@gmail.com>
2024-06-10 00:50:06 -0300

    Sync

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -32,4 +32,14 @@ return {
   },
 
   { "dapt4/vim-autoSurround" },
+
+  {
+    'Exafunction/codeium.vim',
+    config = function ()
+      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  },
 }
